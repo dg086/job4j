fcee84ae2e04919e0092f98a47780accfda78c3d
Dmitry Gaponeko <dg086@mail.ru>
2019-07-14 20:45:11 +0300

    1.3. JavaDoc. Code convention[#157693]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,12 +1,16 @@
 package ru.job4j.calculate;
-
-public class Calculate {   
-   public static void main(String[] args) {
-      System.out.println("Hello, Job4j.");
-   }
-}/**
-* Package for calculate task.
+/**
+* Calculate
 * @author Dmitry Gaponenko (dg086@mail.ru)
 * @version $Id$
 * @since 0.1
-*package ru.job4j.calculate;
+*/
+public class Calculate {
+	/**
+	* main
+	* Hello,job4j
+	*/
+   public static void main(String[] args) {
+      System.out.println("Hello, Job4j.");
+   }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+*
+* @author Dmitry Gaponenko (dg086@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
